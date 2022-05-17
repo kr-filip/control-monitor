7544a6e363c4951eca5bbf3347ab90f67f9b5b96
kr-filip <105446511+kr-filip@users.noreply.github.com>
2022-05-17 15:56:14 +0200

    Added comments, minor adjustments

--- msp430.ino
+++ msp430.ino
@@ -1,41 +1,38 @@
 
-double kp = 4;
-double ki = 0.005;
-double kd = 0.1;
-double out=0;
-double lastError2=0;
-unsigned long previousTime=0;
-double cumError=0; 
-double input;
-float setPoint = 0;
+double kp = 4;  // PID P constant
+double ki = 0.005; // PID I constant
+double kd = 0.1;  // PID D constant
+double lastError2=0; // previous PID error
+unsigned long previousTime=0; //previous time
+double cumError=0; // PID error over time (integral)
+double input; // ADC value 
+float setPoint = 0; // PID input value
 
 void setup() {
-  pinMode(A0, OUTPUT);
+  pinMode(A0, OUTPUT); // PID regulated output pin
   Serial.begin(9600); 
 }
 
 void loop() {
-  input=analogRead(A3);
-    if (Serial.available() > 0) {
+  input=analogRead(A3); // reading capacitor voltage
+    if (Serial.available() > 0) {                       //reading incoming serial data, range 0-255
         float serialValue=Serial.read();
           if (serialValue>=0 && serialValue<=255) {
             setPoint=serialValue;
             setPoint=map(setPoint, 0, 255, 0, 4095);
-          }        
+          }                                            //-------------------------------------------
     }
-  out = computePID(input,setPoint,kp,ki,kd);  
-  analogWrite(A0, out);   
-  Serial.println(adcToVoltage(input,3.3,12));
-  Serial.println(setPoint);
+  analogWrite(A0, computePID(input,setPoint,kp,ki,kd));  //write computed PID output value to pin 
+  Serial.println(adcToVoltage(input,3.3,12));            //send on serial ADC value in volts (capacitor voltage)
   delay(1);
 }
 
-double adcToVoltage(double adcValue, float adcVoltage, float adcBits) {
+double adcToVoltage(double adcValue, float adcVoltage, float adcBits) {   //function that coverts ADC value to volts
   double conversionConstant = adcVoltage/pow(2,adcBits);
   return adcValue * conversionConstant;
-  }
+  }                                                                       //-----------------------------------------
 
-double computePID(double inp, double setPoint, double kp, double ki, double kd){ 
+double computePID(double inp, double setPoint, double kp, double ki, double kd){  // PID controller
   unsigned long currentTime;
   double elapsedTime;
   double error;
@@ -53,4 +50,4 @@ double computePID(double inp, double setPoint, double kp, double ki, double kd){
       out=0;
     } 
   return out;                                        //have function return the PID output
-}
+}                                                                       //---------------------------------------------
