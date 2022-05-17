52384b53e70371c6253fba909f30c83293cef658
kr-filip <105446511+kr-filip@users.noreply.github.com>
2022-05-17 15:35:03 +0200

    Set up hardware an added PID controller

---
+++ msp430.ino
@@ -0,0 +1,48 @@
+
+double kp = 4;
+double ki = 0.005;
+double kd = 0.1;
+double out=0;
+double lastError2=0;
+unsigned long previousTime=0;
+double cumError=0; 
+double input;
+float setPoint = 2000;
+
+void setup() {
+  pinMode(A0, OUTPUT); 
+}
+
+void loop() {
+  input=analogRead(A3);
+  out = computePID(input,setPoint,kp,ki,kd);  
+  analogWrite(A0, out);   
+  Serial.println(adcToVoltage(input,3.3,12));
+  Serial.println(setPoint);
+  delay(1);
+}
+
+double adcToVoltage(double adcValue, float adcVoltage, float adcBits) {
+  double conversionConstant = adcVoltage/pow(2,adcBits);
+  return adcValue * conversionConstant;
+  }
+
+double computePID(double inp, double setPoint, double kp, double ki, double kd){ 
+  unsigned long currentTime;
+  double elapsedTime;
+  double error;
+  double rateError; 
+
+  currentTime = millis();                //get current time
+  elapsedTime = (double)(currentTime - previousTime);        //compute time elapsed from previous computation      
+  error = setPoint - inp;                                // determine error
+  cumError += error * elapsedTime;                // compute integral
+  rateError = (error - lastError2)/elapsedTime;   // compute derivative
+  double out = kp*error + ki*cumError + kd*rateError;                //PID output               
+  lastError2 = error;                                //remember current error
+  previousTime = currentTime;                        //remember current time
+    if (out<0) {
+      out=0;
+    } 
+  return out;                                        //have function return the PID output
+}
