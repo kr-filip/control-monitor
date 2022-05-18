94b775e18ffb264d73e478a4b707d1437ba4dee8
kr-filip <105446511+kr-filip@users.noreply.github.com>
2022-05-18 22:44:51 +0200

    +serial communication, displaying data in a table

--- webApp/app.py
+++ webApp/app.py
@@ -1,6 +1,8 @@
 from threading import Lock
 from flask import Flask, render_template, session, request, jsonify, url_for
 from flask_socketio import SocketIO, emit, disconnect
+import time
+import serial
 
 async_mode = None
 app = Flask(__name__)
@@ -11,33 +13,48 @@ thread_lock = Lock()
 
 def background_thread(args):
     count = 0  
-    dataCounter=0
-
+    dataCounter = 0
+    dataList = []
+    ser = serial.Serial(
+    port='/dev/ttyS0',
+    baudrate =9600,
+    timeout=1
+     )
     while True:
         if args:
           A = dict(args).get('A')
+          #dbV = dict(args).get('db_value')
         else:
           A = 1
-          dbV = 'something'  
-        print(A) 
+          dbV = 'nieco'  
+        print(A)
+        #print(dbV) 
+        #print(args)  
         socketio.sleep(2)
         count += 1
         dataCounter +=1
-         
+        serialData=float(ser.readline())
+#  
+        socketio.emit('my_response',
+                      {'data': serialData, 'count': time.asctime(time.localtime(time.time()))},
+                      namespace='/test')  
+
 @app.route('/')
 def index():
     return render_template('index.html', async_mode=socketio.async_mode)
-
+   
 @socketio.on('my_event', namespace='/test')
 def test_message(message):   
     session['receive_count'] = session.get('receive_count', 0) + 1 
     session['A'] = message['data']    
- 
+    emit('my_response',
+    {'data': message['data'], 'count': session['receive_count']})
+    
 @socketio.on('disconnect_request', namespace='/test')
 def disconnect_request():
     session['receive_count'] = session.get('receive_count', 0) + 1
     emit('my_response',
-         {'data': 'Disconnected!', 'count': session['receive_count']})
+         {'data': 'Disconnected!', 'count': session['receive_count']},namespace='/test')
     disconnect()
     session['A']="Client disconnected"
 
@@ -47,11 +64,13 @@ def test_connect():
     with thread_lock:
         if thread is None:
             thread = socketio.start_background_task(target=background_thread, args=session._get_current_object())
-    emit('my_response', {'data': 'Connected to server', 'count': 0})
+    emit('my_response', {'data': 'Connected to server', 'count': 0},namespace='/test')
+
 
 @socketio.on('disconnect', namespace='/test')
 def test_disconnect():
     print('Client disconnected', request.sid)
+
 
 if __name__ == '__main__':
     socketio.run(app, host="0.0.0.0", port=80, debug=True)
